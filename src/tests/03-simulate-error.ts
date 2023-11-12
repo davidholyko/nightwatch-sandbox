1b9fa03d21dfb084701bda38e5ae06caf12c474e
davidholyko <david.holy.ko@gmail.com>
2023-11-12 14:06:52 -0500

    fix(tests): move package json copy, add comments, dev deps

--- src/tests/03-simulate-error.ts
+++ src/tests/03-simulate-error.ts
@@ -1,6 +1,11 @@
 import 'nightwatch';
 import { DescribeInstance } from 'nightwatch';
 
+/**
+ * This test is designed to fail but there is a TODO to wrap the
+ * simulated error so we can expect the error to occur in the
+ * automation test
+ */
 describe('Simulate Error Test', function (this: DescribeInstance) {
   this.disabled = true;
   this.tags = [];
